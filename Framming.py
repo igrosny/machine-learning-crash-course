8104a89bdb09dc5766760a40f3bf98b8396a7e15
igrosny <igrosny@gmail.com>
2018-06-14 09:34:52 -0300

    Framing - Glosario

---
+++ Framming.py
@@ -0,0 +1,23 @@
+"""
+Supervised Machine learning
+Sistemas de aprendizaje automatico, aprenden como 
+combinar las entras para producir predicciones utiles
+sobre datos que nunca evaluo
+
+Label: Etiqueta, es lo que queremos predecir
+Features: Propiedades, son las variables de entrada
+
+Example: Ejemplo, es una instancia particular de X
+Labeled Example: Ejemplo etiquetado, incluy el vector x y la etiqueta y
+
+Modelos: es la relacion entre las propiedades y la etiqueta
+Primero el model se entrena mostrandoles ejemplos con sus etiqueta
+Y luego se puede utilizar para predecir y'
+
+Los modelos puede ser de regresion o de clasificacion
+para los modelos de regresion van a predecir valores continuos
+como predecir el valor de una casa
+
+Los modelos de clasificacion intentan predecir valores discretos
+(es un mail spam o no)
+"""
