e698d71f127009ea98e8b21ddfdd4329b1b9c724
Ivan <igrosny@gmail.com>
2018-06-18 08:28:22 -0400

    Lo basico de tensorflow

---
+++ Tensorflow.py
@@ -0,0 +1,16 @@
+"""
+Tensorflow es un framework para construir modelos
+Estimators, API orientada a objetos de alto nivel
+tf.layers, tf.losses, tf.metrics: Librerias reutilizables
+para componentes comunes del modelo
+
+tensorflow esta compuesto de dos componentes
+* Un buffer de protocolo de grafos
+* Un aplicativo que ejecuta ese grafo
+
+la analogia con python el modelo es el script
+el runtime el interprete
+
+que api usar? el nivel mas alto que soluciona el problema
+
+tf.estimator API
