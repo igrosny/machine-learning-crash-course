7370938129c776256201d6e413602018ec741fcb
Ivan <igrosny@gmail.com>
2018-06-18 08:10:14 -0400

    disminuir la perdida y tensorflow

--- Loss.py
+++ Loss.py
@@ -51,3 +51,29 @@ el gradiente de la curva de perdida es igual a la derivdad
 
 
 (derivada parciales)
+
+Reduciendo las perdidas: velocidad de prendizaje (learning rate)
+
+el learning rate es el escalar por el cual se multiplica el vector gradiente
+para calcular el proximo punto
+
+Hyperparametros son la perillas que lo programadores podemos
+calibrar en los algoritmos de aprendizaje automatico
+
+the goldilocks principle (el prinicipio de ricitos de oro)
+no es ni frio ni caliente
+
+Nota! en la practica, encontrar el learning rate perfecto no es esencial
+para el correcto entrenamiento de un modelo. tiene que ser
+lo suficientemente bueno para que el modelo converga
+
+Stochastic gradient descent (SGD)
+===========================
+En gradient descente, el batch es el numero total de ejemplos que uso
+para calcular el gradiente en una sola operacion
+
+Stochastic means randomly determined, elije un ejmplo por iteracion
+
+mini batch SGD (entre 10 y 1000 ejmplos elegidos al azar)
+
+
